eabaa696a009b1f1d58f2995b090ed74c9ff802e
Milena Hakobyan <mhakobyan@Milenas-MacBook-Pro.local>
2025-04-22 13:40:47 +0400

    commiting 3rd stage

--- Blockchain with Java/task/src/blockchain/Block.java
+++ Blockchain with Java/task/src/blockchain/Block.java
@@ -4,42 +4,51 @@ import java.util.Date;
 
 public class Block {
     private int id;
-    private String hash; //the hash of a block is a hash of all fields of a block
-    private String prevHash;
-    private long timeStamp;//every block should contain a timestamp representing the time the block was created
+    private final int minerId;
+    private final String hash; //the hash of a block is a hash of all fields of a block
+    private final String prevHash;
+    private final long timeStamp;//every block should contain a timestamp representing the time the block was created
     private int magic;
     private double generationTime;
     private final int numOfZeros;
+    private String difficultyMessage="";
 
-    public Block(String prevBlockHash, int numOfZeros){
+    public Block(String prevBlockHash, int numOfZeros, int minerId){
         this.numOfZeros = numOfZeros;
-        this.id = ++Blockchain.id;
+        this.id = 0;
         this.prevHash = prevBlockHash;
-        this.hash = generateHashCode();
+        this.minerId = minerId;
         this.timeStamp = new Date().getTime();
         this.magic = 0;
+        this.hash = generateHashCode();
+
     }
 
     public String getHash() {
         return hash;
     }
 
-    public void setHash(String hash) { this.hash = hash;}
-
     public int getId() { return id;}
-
-    public void setId(int id) { this.id = id;}
 
     public String getPrevHash() { return prevHash;}
 
-    public void setPrevHash(String prevHash) { this.prevHash = prevHash;}
-
     public long getTimeStamp() { return timeStamp;}
 
-    public void setTimeStamp(long timeStamp) { this.timeStamp = timeStamp;}
+    public void setId(int id){  this.id = id;}
+
+    public void setDifficultyMessage(String s){ difficultyMessage = s;}
 
     public String generateHashCode() {
-        String target = "0".repeat(numOfZeros); // e.g. "0000"
+        if(numOfZeros==0){
+            long startTime = System.currentTimeMillis(); // Start timing
+
+            String hash = StringUtil.applySha256(prevHash + this.id + this.timeStamp + magic);
+
+            long endTime = System.currentTimeMillis(); // End timing
+            generationTime = (endTime - startTime) / 1000.0;
+            return hash;
+        }
+        String target = "0".repeat(numOfZeros); // e.g. "0000", if numZeros==4
         String temp = "";
 
         long startTime = System.currentTimeMillis(); // Start timing
@@ -55,16 +64,18 @@ public class Block {
         return temp;
     }
 
-
-
+    
     //printing a block with the given format
-    public void printBlock() {
+    protected void printBlock() {
         System.out.println("Block:");
-        System.out.println("Id: " + getId());
+        System.out.println("Created by miner # " + minerId);
+        System.out.println("Id: " + id);
         System.out.println("Timestamp: " + getTimeStamp());
+        System.out.println("Magic number: " + magic);
         System.out.println("Hash of the previous block:\n" + getPrevHash());
         System.out.println("Hash of the block:\n" + getHash());
-        System.out.println("Block was generating in " + generationTime + " seconds");
+        System.out.println("Block was generating for " + generationTime + " seconds");
+        System.out.println(difficultyMessage);
         System.out.println();
     }
 }
--- Blockchain with Java/task/src/blockchain/Main.java
+++ Blockchain with Java/task/src/blockchain/Main.java
@@ -1,8 +1,36 @@
 package blockchain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
     public static void main(String[] args) {
-        BlockchainGenerator bg = new BlockchainGenerator();
-        bg.generateBlockchain();
+
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+        Blockchain blockchain = Blockchain.getInstance();
+            try {
+                for (int i = 0; i < 5; i++) {
+                    List<Callable<Block>> callables = new ArrayList<>();
+                    String prevHash = i==0?"0":blockchain.getLastBlock().getHash();
+                    for(int m = 0; m < 10; m++){
+                        int minderId = m + 1; //miner-IDs start from 1
+                        callables.add(()->{
+                            return new Block(prevHash, blockchain.getN(),minderId);
+                        });
+                    }
+                    long startTime = System.currentTimeMillis();
+                    Block block = executor.invokeAny(callables);
+                    long endTime = System.currentTimeMillis();
+                    if(blockchain.validateBlock(block))
+                        blockchain.addBlock(block, endTime - startTime);
+                    }
+                    blockchain.printBlockChain();
+                    executor.shutdown();
+                    executor.awaitTermination(10, TimeUnit.SECONDS);
+                } catch (ExecutionException | InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
     }
 }
